0977ee016a106118722edff8cb2748f544d375c3
unknown <siddharthmehra97@gmail.com>
2020-07-14 15:12:54 -0400

    check content of /c/Users/user/.docker/config.json

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 # Exit in case of error
 set -x
 curl -L https://github.com/kubernetes/kompose/releases/download/v1.18.0/kompose-linux-amd64 -o kompose
-
+cat $HOME/.docker/config.json
 chmod +x kompose
 sudo mv ./kompose /usr/local/bin/kompose
 kompose version

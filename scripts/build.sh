b6248413acdf3180f342660ccbf3bac256aa6758
unknown <siddharthmehra97@gmail.com>
2020-07-14 15:39:30 -0400

    try deployment using kubectl

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,4 +8,5 @@ chmod +x kompose
 sudo mv ./kompose /usr/local/bin/kompose
 kompose version
 kompose convert
-kompose up
+kubectl apply -f backend-deployment.yaml,backend-env-configmap.yaml,celeryworker-deployment.yaml,celeryworker-env-configmap.yaml,db-deployment.yaml,db-env-configmap.yaml,app-db-data-persistentvolumeclaim.yaml,flower-deployment.yaml,flower-env-configmap.yaml,frontend-deployment.yaml,pgadmin-deployment.yaml,pgadmin-env-configmap.yaml,proxy-deployment.yaml,proxy-claim0-persistentvolumeclaim.yaml,queue-deployment.yaml
+

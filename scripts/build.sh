dfd7b08b968d0bb356b376ddf0466b76c1394c18
unknown <siddharthmehra97@gmail.com>
2020-07-16 22:17:38 -0400

    add day1 operations file, update it later

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,6 @@ chmod +x kompose
 sudo mv ./kompose /usr/local/bin/kompose
 kompose version
 kompose convert 
-cat backend-deployment.yaml
 kubectl apply -f backend-deployment.yaml,backend-env-configmap.yaml,celeryworker-deployment.yaml,celeryworker-env-configmap.yaml,db-deployment.yaml,db-env-configmap.yaml,app-db-data-persistentvolumeclaim.yaml,flower-deployment.yaml,flower-env-configmap.yaml,frontend-deployment.yaml,pgadmin-deployment.yaml,pgadmin-env-configmap.yaml,proxy-deployment.yaml,proxy-claim0-persistentvolumeclaim.yaml,queue-deployment.yaml
 kubectl apply -f deployment.yml
 kubectl get deployments

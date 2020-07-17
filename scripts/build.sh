abfa7898416b9d44e017f154e844245ed246f96b
unknown <siddharthmehra97@gmail.com>
2020-07-16 23:15:11 -0400

    add and test the config file

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,6 +9,6 @@ sudo mv ./kompose /usr/local/bin/kompose
 kompose version
 kompose convert 
 kubectl apply -f backend-deployment.yaml,backend-env-configmap.yaml,celeryworker-deployment.yaml,celeryworker-env-configmap.yaml,db-deployment.yaml,db-env-configmap.yaml,app-db-data-persistentvolumeclaim.yaml,flower-deployment.yaml,flower-env-configmap.yaml,frontend-deployment.yaml,pgadmin-deployment.yaml,pgadmin-env-configmap.yaml,proxy-deployment.yaml,proxy-claim0-persistentvolumeclaim.yaml,queue-deployment.yaml
-kubectl apply -f deployment.yml
+kubectl apply -f globally_applied_config_day2_op.yaml
 kubectl get deployments
 kubectl get services -o wide

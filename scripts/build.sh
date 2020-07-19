f16dafbac452e68cdb89ea381f831185b6a899af
unknown <siddharthmehra97@gmail.com>
2020-07-19 17:28:27 -0400

    load balancer configs updated

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,7 @@ kompose version
 kompose convert 
 kubectl apply -f backend-deployment.yaml,backend-env-configmap.yaml,celeryworker-deployment.yaml,celeryworker-env-configmap.yaml,db-deployment.yaml,db-env-configmap.yaml,app-db-data-persistentvolumeclaim.yaml,flower-deployment.yaml,flower-env-configmap.yaml,frontend-deployment.yaml,pgadmin-deployment.yaml,pgadmin-env-configmap.yaml,proxy-deployment.yaml,proxy-claim0-persistentvolumeclaim.yaml,queue-deployment.yaml
 kubectl apply -f globally_applied_config_day2_op.yaml
+kubectl apply -f web-service.yaml
 kubectl apply -f test-config.yaml
 kubectl get deployments
 kubectl get services -o wide
